0abce66fc3b3b6b31af0ee7d02a86bf7dd964ce4
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-11-21 21:22:33 -0200

    [UPDATE] Adding ionic list, input floating, full button and Alert when click on button.

--- src/pages/test/test.ts
+++ src/pages/test/test.ts
@@ -1,12 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the TestPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 
 @IonicPage()
 @Component({
@@ -15,11 +8,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class TestPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad TestPage');
   }
 
+  showAlert() {
+    let alert = this.alertCtrl.create({
+      title: 'Mensagem',
+      subTitle: 'Você logou!',
+      buttons: ['OK']
+    });
+    alert.present();
+  }
 }
