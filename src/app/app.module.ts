7e9197bb8eb8f5294ca0cab42850b3362f465c70
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-11-27 00:49:34 -0200

    [UPDATE] adicionando endpoint de api para consulta de dados http.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpModule } from '@angular/http';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
@@ -10,6 +11,7 @@ import { TestPage } from '../pages/test/test';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+
 @NgModule({
   declarations: [
     MyApp,
@@ -20,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import { TestPage } from '../test/test';
-import { Http } from '@angular/http';
+import { Http, Response } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'page-home',
@@ -9,13 +10,18 @@ import { Http } from '@angular/http';
 })
 export class HomePage {
 
-  private url:string = 'http://localhost:3030/v1';
+  private url:string = 'http://159.203.168.240:8080/district?city_id=1';
+  public beers:Array<{}>;
 
   constructor(
     public navCtrl: NavController,
     public http: Http
   ) {
-      
+      this.http.get(this.url)
+      .map( res => res.json())
+      .subscribe(data => {
+        console.log(data);
+      })
 
   }
 
