2ca3b44b1074a028f76aa1f595d1a91e47faa9bb
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2017-12-19 00:23:59 -0200

    [update] Adding create stuff

--- myFisrtApp/src/pages/list/list.ts
+++ myFisrtApp/src/pages/list/list.ts
@@ -1,38 +1,40 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, NavParams} from 'ionic-angular';
+import {Http, RequestOptions} from '@angular/http';
 
 @Component({
-  selector: 'page-list',
-  templateUrl: 'list.html'
+    selector: 'page-list',
+    templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+    selectedItem: any;
+    icons: string[];
+    private url:string = 'http://localhost:8080/v1';
+    beer = {
+        name: "",
+        price: "",
+        type: "",
+        mark: ""
+    };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+    constructor(
+        public navCtrl: NavController,
+        public navParams: NavParams,
+        public http: Http
+    ) {
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
     }
-  }
 
-  itemTapped(event, item) {
+    saveBeer(beer) {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
 
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
-  }
+        let options = new RequestOptions({ headers : headers });
+        this.http.post(this.url + '/beers', beer, options)
+            .map(res => { res.json() })
+            .subscribe(
+                data => console.log(data)
+            )
+        ;
+    }
 }
