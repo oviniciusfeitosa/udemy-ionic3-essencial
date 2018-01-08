e9d214e7ccae8ace9b3631e36a3a3c80a6260ccb
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2018-01-08 01:46:38 -0200

    [UPDATE] Adding content to block or not if user is not logged in app.

--- myFisrtApp/src/providers/auth/auth.ts
+++ myFisrtApp/src/providers/auth/auth.ts
@@ -1,17 +1,35 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage'
 
-/*
-  Generated class for the AuthProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class AuthProvider {
 
-  constructor(public http: HttpClient) {
+  constructor(
+      public http: HttpClient,
+      public storage: Storage
+  ) {
     console.log('Hello AuthProvider Provider');
+  }
+
+  login() {
+
+  }
+
+  userIsLogged() {
+    this.storage.get('token').then(
+        val => {
+          if(val !== undefined) {
+            return val;
+          } else {
+            return false;
+          }
+        }
+    );
+  }
+
+  logout() {
+    this.storage.remove('token');
   }
 
 }
