d930a6c95041c3ceb111d0b46b1da9f58ffb7831
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2018-01-08 02:04:24 -0200

    [FIX] Resolving errors mapped by JSLint

--- myFisrtApp/src/pages/home/home.ts
+++ myFisrtApp/src/pages/home/home.ts
@@ -1,38 +1,36 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-import { TestPage } from '../test/test';
-import { Http, Response } from '@angular/http';
+import {Component} from '@angular/core';
+import {NavController} from 'ionic-angular';
+import {TestPage} from '../test/test';
+import {Http} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  private url:string = 'http://localhost:8080/v1';
-  public beers:Array<{}>;
+    private url: string = 'http://localhost:8080/v1';
+    public beers: Array<{}>;
 
-  constructor(
-    public navCtrl: NavController,
-    public http: Http
-  ) {
-      this.http.get(this.url + '/beers')
-      .map( res => res.json())
-      .subscribe(data => {
-        this.beers = data;
-      })
+    constructor(public navCtrl: NavController,
+                public http: Http) {
+        this.http.get(this.url + '/beers')
+            .map(res => res.json())
+            .subscribe(data => {
+                this.beers = data;
+            })
 
-  }
+    }
 
-  goToTestPage() {
-    this.navCtrl.push(TestPage);
-  }
+    goToTestPage() {
+        this.navCtrl.push(TestPage);
+    }
 
-  getBeerInfo(id) {
-    this.navCtrl.push(TestPage, {
-      'beer_id' : id,
-      'api_url' : this.url
-    });
-  }
+    getBeerInfo(id) {
+        this.navCtrl.push(TestPage, {
+            'beer_id': id,
+            'api_url': this.url
+        });
+    }
 }
