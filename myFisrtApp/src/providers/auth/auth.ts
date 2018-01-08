5932814d10645c066df9f037cb302ece3d4ec031
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2018-01-08 02:04:48 -0200

    [FIX] Resolving errors mapped by JSLint

--- myFisrtApp/src/providers/auth/auth.ts
+++ myFisrtApp/src/providers/auth/auth.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import 'rxjs/add/operator/map';
 import { Storage } from '@ionic/storage'
 
 @Injectable()
