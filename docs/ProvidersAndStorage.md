8c05562e028090c64e865403ba69019779882bc0
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2018-01-08 01:48:40 -0200

    [UPDATE] Setting imports to Ionic Storage Module at app.module.ts

---
+++ docs/ProvidersAndStorage.md
@@ -0,0 +1,14 @@
+# Providers
+
+Using SQLite:
+* ```sudo ionic cordova plugin add cordova-sqlite-storage```
+
+Using Ionic Storage:
+
+* ```sudo npm install --save @ionic/storage```
+
+Install Auth Provider
+* ```ionic g provider Auth```
+
+Injectables
+* Before class you can declare "@Injectable" to set the class as Injectable and be resolved using Dependency Injection.
--- myFisrtApp/src/app/app.module.ts
+++ myFisrtApp/src/app/app.module.ts
@@ -1,44 +1,47 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpModule } from '@angular/http';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {BrowserModule} from '@angular/platform-browser';
+import {HttpModule} from '@angular/http';
+import {ErrorHandler, NgModule} from '@angular/core';
+import {IonicApp, IonicErrorHandler, IonicModule} from 'ionic-angular';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
-import { TestPage } from '../pages/test/test';
+import {MyApp} from './app.component';
+import {HomePage} from '../pages/home/home';
+import {ListPage} from '../pages/list/list';
+import {TestPage} from '../pages/test/test';
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { Camera } from "@ionic-native/camera";
-import { AuthProvider } from '../providers/auth/auth';
+import {StatusBar} from '@ionic-native/status-bar';
+import {SplashScreen} from '@ionic-native/splash-screen';
+import {Camera} from "@ionic-native/camera";
+import {AuthProvider} from '../providers/auth/auth';
+import {IonicStorageModule} from "@ionic/storage";
 
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage,
-    ListPage,
-    TestPage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-    HttpModule
-  ],
-  bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage,
-    ListPage,
-    TestPage
-  ],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler},
-    Camera,
-    AuthProvider
-  ]
+    declarations: [
+        MyApp,
+        HomePage,
+        ListPage,
+        TestPage
+    ],
+    imports: [
+        BrowserModule,
+        IonicModule.forRoot(MyApp),
+        IonicStorageModule.forRoot({name: '__mydb'}),
+        HttpModule
+    ],
+    bootstrap: [IonicApp],
+    entryComponents: [
+        MyApp,
+        HomePage,
+        ListPage,
+        TestPage
+    ],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: ErrorHandler, useClass: IonicErrorHandler},
+        Camera,
+        AuthProvider
+    ]
 })
-export class AppModule {}
+export class AppModule {
+}
